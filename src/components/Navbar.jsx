93d656540b1c01d6e6fe370991e989a266625217
siddhant pratap <siddhantpratap510@gmail.com>
2023-02-26 18:03:00 +0530

    inittial commit by sid

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import northernLogo from "../assets/pictures/northern_logo.png";
 import "../App.css";
 import { Link } from "react-router-dom";
+import logoNAI from "../assets/pictures/logoNAI.png";
 
 function Navbar() {
   return (
     <div className="navbar">
-      <img className="naiLogo" src={northernLogo} alt="logo" />
+      <div className="nailogoh2">
+        <img className="naiLogo" src={northernLogo} alt="logo" />
+        <img className="naiLOGO" src={logoNAI} alt="logo" />
+      </div>
       <ul>
         <li>
           <Link to="/" className="link">
--- src/pages/BlogAi.jsx
+++ src/pages/BlogAi.jsx
@@ -1,25 +1,38 @@
-import React from 'react';
+import React from "react";
 import BlogAiHeader from "../components/BlogAiHeader";
-import BlogStorySection from "../components/BlogStorySection"
-import { collections } from '../collections';
+import BlogStorySection from "../components/BlogStorySection";
+import { collections } from "../collections";
 
-  function story(collection) {
-    return (<BlogStorySection
-      img={collection.img}
-      title={collection.title}
-    />)
-  }   
+function story(collection) {
+  return <BlogStorySection img={collection.img} title={collection.title} />;
+}
 function BlogAi() {
-
   return (
     <div>
       <BlogAiHeader />
-      <div className='storySection'>
-        {collections.map(story)}
+      <div className="storySection">{collections.map(story)}</div>
+      <div class="loading wave">NORTHERN AI</div>
+      {/* <div class="loading loading-2 wave wave-2">
+        <div class="ff">
+          For browsers that don't support background-clip:text:
+        </div>
+        <div class="loading-2-text"></div>
+      </div> */}
+      <div class="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="rec">
+        <div className="circle"></div>
       </div>
     </div>
-
-  )
+  );
 }
 
 export default BlogAi;
